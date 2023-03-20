9999961e178409ba4f5bdb7048643348926c93d9
1-cool <38838272+1-cool@users.noreply.github.com>
2023-03-20 10:30:12 +0800

    新增union共用体转换

--- test.c
+++ test.c
@@ -1,10 +1,24 @@
 
 #include "stdio.h"
- 
+
 int main(void)
 {
-    unsigned char pMem[] = {0xBF,0xCB,0x88,0x42};
-    float *p = (float*)pMem;
-    printf("%g\r\n",*p);
+    // 指针强制转换
+    unsigned char pMem[] = {0xBF, 0xCB, 0x88, 0x42};
+    float *p = (float *)pMem;
+    printf("%g\r\n", *p);
+    
+    // union共用体
+    union data
+    {
+        unsigned char tmp[4];
+        float t;
+    };
+    union data a;
+    a.tmp[0] = 0xBF;
+    a.tmp[1] = 0xCB;
+    a.tmp[2] = 0x88;
+    a.tmp[3] = 0x42;
+    printf("%f\n", a.t);
     return 0;
 }
